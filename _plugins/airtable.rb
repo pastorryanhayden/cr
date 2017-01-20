15d7c22dcdc4dce29b11f1c6451c422bd4e41be7
Sitesfor.Church <sitesforchurch@gmail.com>
2017-01-19 22:58:03 -0600

    finished menu, added modal styles

---
+++ _plugins/airtable.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'airtable'
+require 'active_support/all'
+
+# Pass in api key to client
+@client = Airtable::Client.new("keyRTLlrVS02vC3Vx")
+# Pass in the app key and table name
+@table = @client.table("appo4EmHX9KELKJ0u", "Photos")
+@records = @table.records(:sort => ["date_added", :desc], :limit => 50)
+
+File.open("_data/photos.json", "w") do |f|
+    f.write(@records.to_json)
+end 
